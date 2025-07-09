46a42871899cee85c094ffe11738ae05a555fe85
marcusandradeinfo <profmarcusandrade@gmail.com>
2025-07-09 19:17:40 -0300

    Alteração do settings.py com o FORCE_SCRIPT_NAME = '/helpme' para forçar o caminho do domínio.

--- app/settings.py
+++ app/settings.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+FORCE_SCRIPT_NAME = '/helpme'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -125,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'helpme/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
